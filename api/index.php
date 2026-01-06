77d8d954e483cfa27614bd175d6738465ca442c4
Paguiel <119151331+paguielng@users.noreply.github.com>
2026-01-06 22:43:43 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
+/**
+ * <?php
+ * header("Location: login.php");
+ * exit;
+ * ?> )
+*/
+
 <?php
-header("Location: login.php");
-exit;
-?>
+require __DIR__ . '/config.php';
+
+if (!isset($_SESSION['auth'])) {
+    require __DIR__ . '/login.php';
+} else {
+    require __DIR__ . '/upload.php';
+}
